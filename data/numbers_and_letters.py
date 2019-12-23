fa78f772171b4788b8c427a7ef777861781876d6
arturcabaj <artur.cabaj@protonmail.com>
2019-12-23 16:00:06 +0100

    First update

---
+++ data/numbers_and_letters.py
@@ -0,0 +1,221 @@
+# Lib to store, you know what
+
+## Digits
+
+digit_zero = [[True, True, True],
+              [True, False, True],
+              [True, False, True],
+              [True, False, True],
+              [True, True, True]]
+
+digit_one = [[False, False, True],
+             [False, False, True],
+             [False, False, True],
+             [False, False, True],
+             [False, False, True]]
+
+digit_two = [[True, True, True],
+             [False, False, True],
+             [True, True, True],
+             [True, False, False],
+             [True, True, True]]
+
+digit_three = [[True, True, True],
+               [False, False, True],
+               [True, True, True],
+               [False, False, True],
+               [True, True, True]]
+
+digit_four = [[True, False, True],
+              [True, False, True],
+              [True, True, True],
+              [False, False, True],
+              [False, False, True]]
+
+digit_five = [[True, True, True],
+              [True, False, False],
+              [True, True, True],
+              [False, False, True],
+              [True, True, True]]
+
+digit_six = [[True, True, True],
+             [True, False, False],
+             [True, True, True],
+             [True, False, True],
+             [True, True, True]]
+
+digit_seven = [[True, True, True],
+               [False, False, True],
+               [False, False, True],
+               [False, False, True],
+               [False, False, True]]
+
+digit_eight = [[True, True, True],
+               [True, False, True],
+               [True, True, True],
+               [True, False, True],
+               [True, True, True]]
+
+digit_nine = [[True, True, True],
+              [True, False, True],
+              [True, True, True],
+              [False, False, True],
+              [True, True, True]]
+
+digits_list = [digit_zero, digit_one, digit_two, digit_three, digit_four, digit_five, digit_six, digit_seven,
+               digit_eight, digit_nine]
+
+## Letters
+
+A = [[False, True, False],
+     [True, False, True],
+     [True, True, True],
+     [True, False, True],
+     [True, False, True]]
+
+B = [[True, True, True],
+     [True, False, True],
+     [True, True, True],
+     [True, False, True],
+     [True, True, True]]
+
+C = [[True, True, True],
+     [True, False, True],
+     [True, True, True],
+     [True, False, True],
+     [True, True, True]]
+
+D = [[True, False, True],
+     [True, False, True],
+     [True, False, True],
+     [True, False, True],
+     [True, True, True]]
+
+E = [[True, True, True],
+     [True, False, False],
+     [True, True, False],
+     [True, False, False],
+     [True, True, True]]
+
+F = [[True, True, True],
+     [True, False, False],
+     [True, True, False],
+     [True, False, False],
+     [True, False, False]]
+
+G = [[True, True, True],
+     [True, False, False],
+     [True, False, True],
+     [True, False, True],
+     [True, False, True]]
+
+H = [[True, False, True],
+     [True, False, True],
+     [True, True, True],
+     [True, False, True],
+     [True, False, True]]
+
+I = [[False, True, False],
+     [False, True, False],
+     [False, True, False],
+     [False, True, False],
+     [False, True, False]]
+
+J = [[False, True, True],
+     [False, False, True],
+     [False, False, True],
+     [False, False, True],
+     [True, True, True]]
+
+K = [[True, False, True],
+     [True, True, False],
+     [True, False, False],
+     [True, True, False],
+     [True, False, True]]
+
+L = [[True, False, False],
+     [True, False, False],
+     [True, False, False],
+     [True, False, True],
+     [True, True, True]]
+
+M = [[True, False, True],
+     [True, True, True],
+     [True, False, True],
+     [True, False, True],
+     [True, False, True]]
+
+N = [[True, False, True],
+     [True, False, True],
+     [True, True, True],
+     [True, False, True],
+     [True, False, True]]
+
+O = [[True, True, True],
+     [True, False, True],
+     [True, False, True],
+     [True, False, True],
+     [True, True, True]]
+
+P = [[True, True, True],
+     [True, False, True],
+     [True, True, True],
+     [True, False, False],
+     [True, False, False]]
+
+R = [[True, True, True],
+     [True, False, True],
+     [True, True, True],
+     [True, False, True],
+     [True, False, True]]
+
+S = [[True, True, True],
+     [True, False, False],
+     [True, True, True],
+     [False, False, True],
+     [True, True, True]]
+
+T = [[True, True, True],
+     [False, True, False],
+     [False, True, False],
+     [False, True, False],
+     [False, True, False]]
+
+U = [[True, False, True],
+     [True, False, True],
+     [True, False, True],
+     [True, False, True],
+     [True, True, True]]
+
+V = [[True, False, True],
+     [True, False, True],
+     [True, False, True],
+     [False, False, False],
+     [False, True, False]]
+
+W = [[True, False, True],
+     [True, False, True],
+     [True, False, True],
+     [True, True, True],
+     [True, False, True]]
+
+Q = [[True, True, True],
+     [True, False, True],
+     [True, False, True],
+     [True, True, True],
+     [True, True, True]]
+
+X = [[True, False, True],
+     [True, False, True],
+     [False, True, False],
+     [True, False, True],
+     [True, False, True]]
+
+Z = [[True, True, True],
+     [False, False, True],
+     [False, True, False],
+     [True, False, False],
+     [True, True, True]]
+
+
+letters_list = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z]
---
+++ rasmat/rasmat.py
@@ -0,0 +1,24 @@
+import numpy as np
+import data.numbers_and_letters
+
+import argparse     # work in terminal
+
+from neopixel import *
+from copy import deepcopy
+import time
+
+# LED strip configuration:
+LED_COUNT = 100  # Number of LED pixels.
+LED_PIN = 10  # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0 (must be in directory!!! find it)).
+LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 10  # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 50  # Set to 0 for darkest and 255 for brightest
+LED_INVERT = False  # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL = 0  # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+
+strip.begin()
+for i in range(LED_COUNT):
+    strip.setPixelColor(i, Color(255,0,0))
+    time.sleep(1)
