3c8b6aea523eadf5fd3b5cb9f99c6acaef374698
arturcabaj <artur.cabaj@protonmail.com>
2019-12-24 21:58:04 +0100

    Update rasmat.py

--- rasmat.py
+++ rasmat.py
@@ -294,7 +294,6 @@ if __name__ == '__main__':
     # try:
         while True:
             newClock.clock(strip)
-            strip.setBrightness(5)
     # except KeyboardInterrupt:
     #     observer.stop()
 
