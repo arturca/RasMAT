8d67336a552129e3b2d731216c06006313d8f7a7
arturcabaj <artur.cabaj@protonmail.com>
2020-02-06 12:48:04 +0100

    Update clock_file.py

--- data/clock_file.py
+++ data/clock_file.py
@@ -58,7 +58,8 @@ class Clock:
             else:
                 # create 'space line'
                 self.queue = np.column_stack((self.queue, np.array([[False], [False], [False], [False], [False]])))
-                if 0 < ord(letter) - 65 < len(digits_and_letters.letters_list):
+                if 0 <= ord(letter) - 65 < len(digits_and_letters.letters_list):
+                    print(letter)
                     self.queue = np.column_stack((self.queue, np.array(digits_and_letters.letters_list[ord(letter) - 65])))
 
         self.paint_scrolling_name()
